b1a6a212ab026c62a36fa28b799af231ff136530
cir.no <cirno@cognant.tech>
2022-01-02 17:35:34 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/realeu/tgvc-userbot /root/realeu
-cp .env /root/realeu/.env
-cd /root/realeu
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t tgvc-userbot
-docker run --privileged --env-file .env --rm -i tgvc-userbot
+git clone https://github.com/subinps/VCPlayerBot /root/subinps
+cp .env /root/subinps/.env
+cd /root/subinps
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t VCPlayerBot
+docker run --privileged --env-file .env --rm -i VCPlayerBot
