bb8f503aae6ad4c5eccd2def43df067b061dc766
cir.no <cirno@cognant.tech>
2022-01-02 17:00:38 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/me-piro-786/tgvc-userbot /root/TGVC 
-cp musicplayer/.env /root/TGVC 
+cp tgvc-userbot/.env /root/TGVC 
 cd /root/TGVC 
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t musicplayer
-docker run --privileged --env-file .env --rm -i musicplayer 
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t tgvc-userbot
+docker run --privileged --env-file .env --rm -i tgvc-userbot 
